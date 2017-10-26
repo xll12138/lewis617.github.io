8395c965e3ecfe6bef2f8961fbd2a09cadd4ba38
tianke.lyq <tianke.lyq@alibaba-inc.com>
2017-10-26 19:27:54 +0800

    change disqus-proxy from react to jq && add gulp build

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+var gulp = require('gulp');
+var minifycss = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlclean = require('gulp-htmlclean');
+var imagemin = require('gulp-imagemin');
+var gutil = require('gulp-util')
+var del = require('del');
+var runSequence = require('run-sequence');
+var Hexo = require('hexo');
+
+
+gulp.task('clean', function() {
+    return del(['public/**/*']);
+});
+
+// generate html with 'hexo generate'
+var hexo = new Hexo(process.cwd(), {});
+gulp.task('generate', function(cb) {
+    hexo.init().then(function() {
+        return hexo.call('generate', {
+            watch: false
+        });
+    }).then(function() {
+        return hexo.exit();
+    }).then(function() {
+        return cb()
+    }).catch(function(err) {
+        console.log(err);
+        hexo.exit(err);
+        return cb(err);
+    })
+})
+
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('minify-html', function() {
+    return gulp.src('./public/**/*.html')
+        .pipe(htmlclean())
+        .pipe(htmlmin({
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+        }))
+        .pipe(gulp.dest('./public'))
+});
+
+gulp.task('minify-js', function() {
+    return gulp.src('./public/**/*.js')
+        .pipe(uglify())
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+        .pipe(gulp.dest('./public'));
+});
+
+gulp.task('minify-img', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('minify-img-aggressive', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin(
+        [imagemin.gifsicle({'optimizationLevel': 3}), 
+        imagemin.jpegtran({'progressive': true}), 
+        imagemin.optipng({'optimizationLevel': 7}), 
+        imagemin.svgo()],
+        {'verbose': true}))
+        .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('compress', function(cb) {
+    runSequence(['minify-html', 'minify-css', 'minify-js', 'minify-img-aggressive'], cb);
+});
+
+gulp.task('build', function(cb) {
+    runSequence('clean', 'generate', 'compress', cb)
+});
+
+gulp.task('default', ['build'])
