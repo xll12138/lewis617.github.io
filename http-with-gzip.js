768b3816387a04b593d91bbd68649f209d5107d8
tianke.lyq <tianke.lyq@alibaba-inc.com>
2017-10-31 09:42:20 +0800

    Fix bug of directory

--- http-with-gzip.js
+++ http-with-gzip.js
@@ -4,7 +4,7 @@
 //
 
 var DOCUMENT_ROOT = './public';
-var DIRECTORY_INDEX = '/index.html';
+var DIRECTORY_INDEX = 'index.html';
 
 var port = process.env.PORT || 8080;
 
@@ -14,57 +14,41 @@ var path = require('path');
 var fs = require('fs');
 var mime = require('mime');
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
+  if (request.url.indexOf('?') > -1) {
+    request.url = request.url.substr(0, request.url.indexOf('?'));
+  }
+  var filePath = DOCUMENT_ROOT + request.url;
+  if (!fs.existsSync(filePath)) {
+    response.writeHead(404);
+    response.end();
+  } else {
+    if (fs.statSync(filePath).isDirectory()) {
+      filePath = path.join(filePath, DIRECTORY_INDEX);
+    }
+    var acceptEncoding = request.headers['accept-encoding'] || '';
+    fs.readFile(filePath, function (error, content) {
+      if (error) {
+        response.writeHead(500);
+        response.end();
+      }
+      else {
+        var raw = fs.createReadStream(filePath);
 
-	// Remove query strings from uri
-	if (request.url.indexOf('?')>-1) {
-		request.url = request.url.substr(0, request.url.indexOf('?'));
-	}
-
-	// Remove query strings from uri
-	if (request.url == '/') {
-		request.url = DIRECTORY_INDEX;
-	}
-	var filePath = DOCUMENT_ROOT + request.url;
-
-	var extname = path.extname(filePath);
-
-	var acceptEncoding = request.headers['accept-encoding'];
-	if (!acceptEncoding) {
-		acceptEncoding = '';
-	}
-
-	fs.exists(filePath, function(exists) {
-
-		if (exists) {
-			fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-          var raw = fs.createReadStream(filePath);
-          
-          response.setHeader("Content-Type", mime.getType(filePath));
-					if (acceptEncoding.match(/\bdeflate\b/)) {
-						response.writeHead(200, { 'content-encoding': 'deflate' });
-						raw.pipe(zlib.createDeflate()).pipe(response);
-					} else if (acceptEncoding.match(/\bgzip\b/)) {
-						response.writeHead(200, { 'content-encoding': 'gzip' });
-						raw.pipe(zlib.createGzip()).pipe(response);
-					} else {
-						response.writeHead(200, {});
-						raw.pipe(response);
-					}
-				}
-			});
-		}
-		else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-
+        response.setHeader("Content-Type", mime.getType(filePath));
+        if (acceptEncoding.match(/\bdeflate\b/)) {
+          response.writeHead(200, { 'content-encoding': 'deflate' });
+          raw.pipe(zlib.createDeflate()).pipe(response);
+        } else if (acceptEncoding.match(/\bgzip\b/)) {
+          response.writeHead(200, { 'content-encoding': 'gzip' });
+          raw.pipe(zlib.createGzip()).pipe(response);
+        } else {
+          response.writeHead(200, {});
+          raw.pipe(response);
+        }
+      }
+    });
+  }
 }).listen(port);
 
 console.log('Serving files on http://localhost:' + port);
